97b5cefc9beed496780c82275708b56c6b44d3f7
viemo777 <viemo777@gmail.com>
2022-10-10 18:05:06 +0300

    Initial commit

---
+++ dictionaries.py
@@ -0,0 +1,23 @@
+car_prices = {'opel': 12, 'audi': 45, 'bmw': 10000}
+print(car_prices['opel'])
+print(car_prices.get('opel'))
+car_prices['skoda'] = 30000
+print(car_prices)
+
+car_prices['opel'] = 2380
+print(car_prices)
+
+# Dictionaries
+# x= {'key1': value1, } # ex. {'color': 'Green', 'size': 32}
+# x['key1'] # ex. x['color'] # => 'Green'
+# ['key2'] = 'value2'  # ex. x['brand'] = 'zara' # => {'color': 'Green', 'size': 32, 'brand': 'zara'}
+# del dictname['key3']  # ex. del x['color'] # => {'size': 32, 'brand': 'zara'}
+# dictname.clear())  # ex. x.clear() # => {}
+
+
+# Lists
+# x = [arg1, arg2, ] # ex. x = [12, 32.43, 'WWW']
+# x[index]  # ex. x[2] # =>  'WWW'
+# append(item)  # ex. x.append('AAA') # =>  x = [12, 32.43, 'WWW', 'AAA']
+# insert(index, item)  # ex. x.insert(2, 'III') # =>  x = [12, 32.43, 'WWW', 'III']
+# pop(index) # ex. x.pop(2) # =>  x = [12, 32.43, 'III']
